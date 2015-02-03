dceaee991b1bc5055f4ac8dce73f6846fc4843bc
Diego Giagio <diego@giagio.com>
2015-02-02 22:26:00 -0200

    Add pluggable HTTP connection implementations support

---
+++ src/main/java/com/suse/saltstack/netapi/client/SaltStackAPIConstants.java
@@ -0,0 +1,10 @@
+package com.suse.saltstack.netapi.client;
+
+/**
+ * Constants used in SaltStack API.
+ */
+public class SaltStackAPIConstants {
+    /** Known values for 'eauth' parameter when logging in */
+    public static final String LOGIN_EAUTH_AUTO = "auto";
+    public static final String LOGIN_EAUTH_PAM = "pam";
+}
