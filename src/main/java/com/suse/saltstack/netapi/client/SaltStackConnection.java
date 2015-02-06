60cef9c37e58b9c07fb399d0741700b07cd8026f
Diego Giagio <diego@giagio.com>
2015-02-06 08:50:29 -0200

    Added Javadoc to SaltStackConnection interface methods.

--- src/main/java/com/suse/saltstack/netapi/client/SaltStackConnection.java
+++ src/main/java/com/suse/saltstack/netapi/client/SaltStackConnection.java
@@ -8,5 +8,13 @@ import java.lang.reflect.Type;
  * Describes an interface for different HTTP connection implementations.
  */
 public interface SaltStackConnection {
+    /**
+     * Send a POST request and parse the result into object of given {@link Type}.
+     *
+     * @param resultType the type of the result
+     * @param data the data to send (in JSON format)
+     * @return object of type given by resultType
+     * @throws SaltStackException if the request was not successful
+     */
     <T> T getResult(Type resultType, String data) throws SaltStackException;
 }
