aaff661da80a6d6ce318de3f2d9ab112b46ef6a9
Sonja Krause-Harder <skh@rcane.de>
2015-02-17 18:25:50 +0100

    Add javadoc to SaltStackRunResults

--- src/main/java/com/suse/saltstack/netapi/results/SaltStackRunResults.java
+++ src/main/java/com/suse/saltstack/netapi/results/SaltStackRunResults.java
@@ -5,11 +5,19 @@ import java.util.Map;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * Models the netapi response from the /run endpoint
+ */
+
 public class SaltStackRunResults {
 
     @SerializedName("return")
     private List<Map<String, String>> results;
 
+    /**
+     * @return A Map of minion names and command results for each minion,
+     *         wrapped in a list with one element.
+     */
     public List<Map<String, String>> getResults() {
         return results;
     }
