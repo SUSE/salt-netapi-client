873cdc8e5b62fd83f7cc0c35f472514d2adb1dee
Kevin Walter <kevin.walter.private@googlemail.com>
2015-02-19 17:34:17 +0100

    Rename SaltStackParserTest to JsonParserTest

--- src/test/java/com/suse/saltstack/netapi/parser/JsonParserTest.java
+++ src/test/java/com/suse/saltstack/netapi/parser/JsonParserTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class SaltStackParserTest {
+public class JsonParserTest {
 
     @Test
     public void testSaltStackJobParser() throws Exception {
