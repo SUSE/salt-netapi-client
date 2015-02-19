c8638b412a67a76d95e2002b9d7d67ddb8a09e75
Kevin Walter <kevin.walter.private@googlemail.com>
2015-02-19 18:21:33 +0100

    Rename SaltStackParsingException to ParsingException

--- src/main/java/com/suse/saltstack/netapi/exception/ParsingException.java
+++ src/main/java/com/suse/saltstack/netapi/exception/ParsingException.java
@@ -3,13 +3,13 @@ package com.suse.saltstack.netapi.exception;
 /**
  * Exception to be thrown in case of problems parsing service responses.
  */
-public class SaltStackParsingException extends SaltStackException {
+public class ParsingException extends SaltStackException {
     /**
      * Constructor expecting a custom cause.
      *
      * @param cause the cause
      */
-    public SaltStackParsingException(Throwable cause) {
+    public ParsingException(Throwable cause) {
         super(cause);
     }
 
@@ -18,7 +18,7 @@ public class SaltStackParsingException extends SaltStackException {
      *
      * @param message the message
      */
-    public SaltStackParsingException(String message) {
+    public ParsingException(String message) {
         super(message);
     }
 
