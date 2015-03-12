86fbf6a04c1e352d6386c51d410cf1e42c160d44
Frantisek Kobzik <fkobzik@redhat.com>
2015-03-12 20:07:32 +0100

    Checkstyle: comply to JavadocType
    TODO: check TODOs in Stats-related classes

--- src/main/java/com/suse/saltstack/netapi/datatypes/Job.java
+++ src/main/java/com/suse/saltstack/netapi/datatypes/Job.java
@@ -2,6 +2,9 @@ package com.suse.saltstack.netapi.datatypes;
 
 import java.util.List;
 
+/**
+ * Representation of a list of minions associated with given job.
+ */
 public class Job {
 
     private String jid;
