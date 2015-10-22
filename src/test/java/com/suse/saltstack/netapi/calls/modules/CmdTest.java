2fc0fe25eba44a7b87f9e3a0e528e29835fa2b68
Michele Bologna <michele.bologna@gmail.com>
2015-10-22 21:21:05 +0200

    Add tests for `cmd' module
    - Add test for cmd.run with the output of the
    'uptime command'

---
+++ src/test/java/com/suse/saltstack/netapi/calls/modules/CmdTest.java
@@ -0,0 +1,28 @@
+package com.suse.saltstack.netapi.calls.modules;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.InputStream;
+
+import org.junit.Test;
+
+import com.google.gson.reflect.TypeToken;
+import com.suse.saltstack.netapi.parser.JsonParser;
+
+/**
+ * Cmd unit tests.
+ */
+public class CmdTest {
+
+    @Test
+    public void testCmdUptime() {
+        TypeToken<String> type = Cmd.run("uptime").getReturnType();
+        InputStream is = this.getClass()
+                .getResourceAsStream("/modules/cmd/uptime.json");
+
+        JsonParser<String> parser = new JsonParser<>(type);
+        String parsed = parser.parse(is);
+        assertEquals(" 16:51:23 up 22 min,  0 users,  load average: 0.00, 0.02, 0.09",
+                parsed);
+    }
+}
