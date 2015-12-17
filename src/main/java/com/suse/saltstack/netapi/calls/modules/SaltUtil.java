53a231b860cec669a940d1ad6d15e93dae31b802
Matei Albu <malbu@suse.de>
2015-12-17 14:35:01 +0100

    add sync_modules and sync_all

--- src/main/java/com/suse/saltstack/netapi/calls/modules/SaltUtil.java
+++ src/main/java/com/suse/saltstack/netapi/calls/modules/SaltUtil.java
@@ -2,6 +2,7 @@ package com.suse.saltstack.netapi.calls.modules;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import com.google.gson.reflect.TypeToken;
@@ -12,11 +13,34 @@ import com.suse.saltstack.netapi.calls.LocalCall;
  */
 public class SaltUtil {
 
-    public static LocalCall<List<String>> syncGrains() {
-        LinkedHashMap<String, Object> args = new LinkedHashMap<>();
-        return new LocalCall("saltutil.sync_grains", Optional.empty(),
+    public static LocalCall<List<String>> syncGrains(String saltenv, boolean refresh) {
+        LinkedHashMap<String, Object> args = syncArgs(saltenv, refresh);
+        return new LocalCall<>("saltutil.sync_grains", Optional.empty(),
                 Optional.of(args), new TypeToken<List<String>>() {
                 });
+    }
+
+    public static LocalCall<List<String>> syncModules(String saltenv, boolean refresh) {
+        LinkedHashMap<String, Object> args = syncArgs(saltenv, refresh);
+        return new LocalCall<>("saltutil.sync_modules", Optional.empty(),
+                Optional.of(args), new TypeToken<List<String>>() {
+                });
+    }
+
+    public static LocalCall<Map<String, Object>> syncAll(String saltenv, boolean refresh) {
+        LinkedHashMap<String, Object> args = syncArgs(saltenv, refresh);
+        return new LocalCall<>("saltutil.sync_all", Optional.empty(),
+                Optional.of(args), new TypeToken<Map<String, Object>>() {
+                });
+    }
+
+    private static LinkedHashMap<String, Object> syncArgs(String saltenv, boolean refresh) {
+        LinkedHashMap<String, Object> args = new LinkedHashMap<>();
+        if (saltenv != null) {
+            args.put("saltenv", saltenv);
+        }
+        args.put("refresh", refresh);
+        return args;
     }
 
 }
--- src/test/java/com/suse/saltstack/netapi/calls/modules/SaltUtilTest.java
+++ src/test/java/com/suse/saltstack/netapi/calls/modules/SaltUtilTest.java
@@ -5,6 +5,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.any;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.net.HttpURLConnection;
 import java.net.URI;
@@ -31,6 +32,12 @@ public class SaltUtilTest {
     static final String JSON_SYNCGRAINS_RESPONSE = ClientUtils.streamToString(
             SaltUtilTest.class.getResourceAsStream("/modules/saltutil/syncgrains.json"));
 
+    static final String JSON_SYNCMODULES_RESPONSE = ClientUtils.streamToString(
+            SaltUtilTest.class.getResourceAsStream("/modules/saltutil/syncmodules.json"));
+
+    static final String JSON_SYNCALL_RESPONSE = ClientUtils.streamToString(
+            SaltUtilTest.class.getResourceAsStream("/modules/saltutil/syncall.json"));
+
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(MOCK_HTTP_PORT);
 
@@ -50,12 +57,54 @@ public class SaltUtilTest {
                 .withHeader("Content-Type", "application/json")
                 .withBody(JSON_SYNCGRAINS_RESPONSE)));
 
-        Map<String, List<String>> response = SaltUtil.syncGrains()
-                .callSync(client, new MinionList("localhost"));
+        Map<String, List<String>> response = SaltUtil.syncGrains(null, true)
+                .callSync(client, new MinionList("minion1"));
 
         assertEquals(response.size(), 1);
-        assertEquals(response.entrySet().iterator().next().getKey(), "localhost");
+        assertEquals(response.entrySet().iterator().next().getKey(), "minion1");
         assertEquals(response.entrySet().iterator().next().getValue().size(), 0);
+    }
+
+    @Test
+    public void testSyncModules() throws SaltStackException {
+        stubFor(any(urlMatching("/"))
+                .willReturn(aResponse()
+                .withStatus(HttpURLConnection.HTTP_OK)
+                .withHeader("Content-Type", "application/json")
+                .withBody(JSON_SYNCMODULES_RESPONSE)));
+
+        Map<String, List<String>> response = SaltUtil.syncModules(null, true)
+                .callSync(client, new MinionList("minion1"));
+
+        assertEquals(response.size(), 1);
+        assertEquals(response.entrySet().iterator().next().getKey(), "minion1");
+        assertEquals(response.entrySet().iterator().next().getValue().size(), 0);
+    }
+
+    @Test
+    public void testSyncAll() throws SaltStackException {
+        stubFor(any(urlMatching("/"))
+                .willReturn(aResponse()
+                .withStatus(HttpURLConnection.HTTP_OK)
+                .withHeader("Content-Type", "application/json")
+                .withBody(JSON_SYNCALL_RESPONSE)));
+
+        Map<String, Map<String, Object>> response = SaltUtil.syncAll(null, true)
+                .callSync(client, new MinionList("minion1"));
+
+        assertEquals(response.size(), 1);
+        assertNotNull(response.get("minion1"));
+        Map<String, Object> data = response.get("minion1");
+        assertEquals(((List) data.get("beacons")).size(), 0);
+        assertEquals(((List) data.get("grains")).size(), 0);
+        assertEquals(((List) data.get("log_handlers")).size(), 0);
+        assertEquals(((List) data.get("modules")).size(), 0);
+        assertEquals(((List) data.get("output")).size(), 0);
+        assertEquals(((List) data.get("proxymodules")).size(), 0);
+        assertEquals(((List) data.get("renderers")).size(), 0);
+        assertEquals(((List) data.get("returners")).size(), 0);
+        assertEquals(((List) data.get("states")).size(), 0);
+        assertEquals(((List) data.get("utils")).size(), 0);
     }
 
 }
