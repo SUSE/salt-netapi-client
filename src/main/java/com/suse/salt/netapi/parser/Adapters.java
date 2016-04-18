d67dc0c23881e63fb6b0076f1c4c3c6076fec1b6
Kevin Walter <kwalter@suse.com>
2016-04-18 16:32:40 +0200

    add strict parser for String and Boolean

---
+++ src/main/java/com/suse/salt/netapi/parser/Adapters.java
@@ -0,0 +1,41 @@
+package com.suse.salt.netapi.parser;
+
+import com.google.gson.JsonParseException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+
+class Adapters {
+    static final TypeAdapter<Boolean> BOOLEAN = new TypeAdapter<Boolean>() {
+        @Override
+        public Boolean read(JsonReader in) throws IOException {
+            return in.nextBoolean();
+        }
+        @Override
+        public void write(JsonWriter out, Boolean value) throws IOException {
+            if (value == null) {
+                throw new JsonParseException("null is not a valid value for boolean");
+            } else {
+                out.value(value);
+            }
+        }
+    };
+
+    static final TypeAdapter<String> STRING = new TypeAdapter<String>() {
+        @Override
+        public String read(JsonReader in) throws IOException {
+            return in.nextString();
+        }
+        @Override
+        public void write(JsonWriter out, String value) throws IOException {
+            if (value == null) {
+                throw new JsonParseException("null is not a valid value for string");
+            } else {
+                out.value(value);
+            }
+        }
+    };
+}
