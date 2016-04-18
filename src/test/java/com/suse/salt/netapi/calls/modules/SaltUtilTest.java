70e42f3dbf2734ac05467f434f5b8cd284745ba9
Kevin Walter <kwalter@suse.com>
2016-04-18 16:37:44 +0200

    adjust tests for error handling

--- src/test/java/com/suse/salt/netapi/calls/modules/SaltUtilTest.java
+++ src/test/java/com/suse/salt/netapi/calls/modules/SaltUtilTest.java
@@ -13,6 +13,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import com.suse.salt.netapi.results.SaltError;
+import com.suse.salt.netapi.utils.Xor;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -59,13 +61,13 @@ public class SaltUtilTest {
                 .withHeader("Content-Type", "application/json")
                 .withBody(JSON_SYNCGRAINS_RESPONSE)));
 
-        Map<String, List<String>> response = SaltUtil
+        Map<String, Xor<SaltError, List<String>>> response = SaltUtil
                 .syncGrains(Optional.of(true), Optional.empty())
                 .callSync(client, new MinionList("minion1"));
 
         assertEquals(1, response.size());
         assertEquals("minion1", response.entrySet().iterator().next().getKey());
-        assertEquals(0, response.entrySet().iterator().next().getValue().size());
+        assertEquals(0, response.entrySet().iterator().next().getValue().right().get().size());
     }
 
     @Test
@@ -76,13 +78,13 @@ public class SaltUtilTest {
                 .withHeader("Content-Type", "application/json")
                 .withBody(JSON_SYNCMODULES_RESPONSE)));
 
-        Map<String, List<String>> response = SaltUtil
+        Map<String, Xor<SaltError, List<String>>> response = SaltUtil
                 .syncModules(Optional.of(true), Optional.empty())
                 .callSync(client, new MinionList("minion1"));
 
         assertEquals(1, response.size());
         assertEquals("minion1", response.entrySet().iterator().next().getKey());
-        assertEquals(0, response.entrySet().iterator().next().getValue().size());
+        assertEquals(0, response.entrySet().iterator().next().getValue().right().get().size());
     }
 
     @Test
@@ -93,13 +95,13 @@ public class SaltUtilTest {
                 .withHeader("Content-Type", "application/json")
                 .withBody(JSON_SYNCALL_RESPONSE)));
 
-        Map<String, Map<String, Object>> response = SaltUtil
+        Map<String, Xor<SaltError, Map<String, Object>>> response = SaltUtil
                 .syncAll(Optional.of(true), Optional.empty())
                 .callSync(client, new MinionList("minion1"));
 
         assertEquals(1, response.size());
         assertNotNull(response.get("minion1"));
-        Map<String, Object> data = response.get("minion1");
+        Map<String, Object> data = response.get("minion1").right().get();
         assertEquals(0, ((List<?>) data.get("beacons")).size());
         assertEquals(0, ((List<?>) data.get("grains")).size());
         assertEquals(0, ((List<?>) data.get("log_handlers")).size());
