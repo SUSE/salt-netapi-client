4dda4198ba719b2d48c6275c416dd873a105a004
Kevin Walter <kwalter@suse.com>
2016-04-22 11:26:35 +0200

    context specific data in events can be now parsed in a staged manner

--- src/main/java/com/suse/salt/netapi/datatypes/Event.java
+++ src/main/java/com/suse/salt/netapi/datatypes/Event.java
@@ -1,6 +1,11 @@
 package com.suse.salt.netapi.datatypes;
 
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+
 import java.util.Map;
+
+import static com.suse.salt.netapi.parser.JsonParser.GSON;
 
 /**
  * Parse events into objects.
@@ -8,7 +13,7 @@ import java.util.Map;
 public class Event {
 
     private String tag;
-    private Map<String, Object> data;
+    private JsonElement data;
 
     /**
      * Return this event's tag.
@@ -20,9 +25,30 @@ public class Event {
 
     /**
      * Return this event's data.
+     * @param dataType type token to parse data
+     * @param <R> type to parse the data into
+     * @return the data
+     */
+    public <R> R getData(TypeToken<R> dataType) {
+        return GSON.fromJson(data, dataType.getType());
+    }
+
+    /**
+     * Return this event's data parsed into the given type.
+     * @param dataType class to parse data
+     * @param <R> type to parse the data into
+     * @return the data
+     */
+    public <R> R getData(Class<R> dataType) {
+        return GSON.fromJson(data, dataType);
+    }
+
+    /**
+     * Return this event's data as a Map
      * @return the data
      */
     public Map<String, Object> getData() {
-        return data;
+        TypeToken<Map<String, Object>> typeToken = new TypeToken<Map<String, Object>>() {};
+        return GSON.fromJson(data, typeToken.getType());
     }
 }
--- src/main/java/com/suse/salt/netapi/event/ResultEvent.java
+++ src/main/java/com/suse/salt/netapi/event/ResultEvent.java
@@ -1,6 +1,10 @@
 package com.suse.salt.netapi.event;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
 import com.suse.salt.netapi.datatypes.Event;
+import com.suse.salt.netapi.parser.JsonParser;
 
 import java.util.Map;
 import java.util.Optional;
@@ -15,9 +19,11 @@ public class ResultEvent {
     private static final Pattern PATTERN =
             Pattern.compile("^salt/job/(\\d{20})/ret/([^/]+)$");
 
+    private static final Gson GSON = JsonParser.GSON;
+
     private final String jid;
     private final String minionId;
-    private final Map<String, Object> data;
+    private final JsonElement data;
 
     /**
      * Creates a new ResultEvent
@@ -25,7 +31,7 @@ public class ResultEvent {
      * @param minionId the id of the minion sending the event
      * @param data data containing more information about this event
      */
-    public ResultEvent(String jid, String minionId, Map<String, Object> data) {
+    public ResultEvent(String jid, String minionId, JsonElement data) {
         this.jid = jid;
         this.minionId = minionId;
         this.data = data;
@@ -49,13 +55,34 @@ public class ResultEvent {
         return minionId;
     }
 
+
     /**
-     * The event data containing more information about this event
-     *
-     * @return the event data
+     * Return event data as Map
+     * @return event data as map
      */
     public Map<String, Object> getData() {
-        return data;
+        TypeToken<Map<String, Object>> typeToken = new TypeToken<Map<String, Object>>() {};
+        return getData(typeToken);
+    }
+
+    /**
+     * Return the event data parsed into the given type.
+     * @param type type token to parse data
+     * @param <R> type to parse the data into
+     * @return the event data
+     */
+    public <R> R getData(TypeToken<R> type) {
+        return GSON.fromJson(data, type.getType());
+    }
+
+    /**
+     * Return this event's data parsed into the given type.
+     * @param type class to parse data
+     * @param <R> type to parse the data into
+     * @return the data
+     */
+    public <R> R getData(Class<R> type) {
+        return GSON.fromJson(data, type);
     }
 
     /**
@@ -67,7 +94,7 @@ public class ResultEvent {
         Matcher matcher = PATTERN.matcher(event.getTag());
         if (matcher.matches()) {
             ResultEvent result = new ResultEvent(matcher.group(1),
-                    matcher.group(2), event.getData());
+                    matcher.group(2), event.getData(JsonElement.class));
             return Optional.of(result);
         } else {
             return Optional.empty();
