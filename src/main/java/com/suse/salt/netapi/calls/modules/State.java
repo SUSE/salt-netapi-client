fe071ebb21a72769aadeaa2334b5c35dad534a54
Matei Albu <malbu@suse.de>
2016-05-04 12:05:57 +0200

    apply state with pillar data and show_highstate

--- src/main/java/com/suse/salt/netapi/calls/modules/State.java
+++ src/main/java/com/suse/salt/netapi/calls/modules/State.java
@@ -5,6 +5,7 @@ import com.suse.salt.netapi.calls.LocalCall;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,6 +27,20 @@ public class State {
 
     public static LocalCall<Map<String, Object>> apply(String... mods) {
         return apply(Arrays.asList(mods));
+    }
+
+    public static LocalCall<Map<String, Object>> apply(String mod,
+            Map<String, Object> pillar) {
+        LinkedHashMap args = new LinkedHashMap();
+        args.put("mods", Collections.singletonList(mod));
+        args.put("pillar", pillar);
+        return new LocalCall("state.apply", Optional.empty(), Optional.of(args),
+                new TypeToken<Map<String, Object>>() { });
+    }
+
+    public static LocalCall<Object> showHighstate() {
+        return new LocalCall<>("state.show_highstate", Optional.empty(), Optional.empty(),
+                new TypeToken<Object>() { });
     }
 
 }
