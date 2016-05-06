2cca77f733c92ca9fca95acf93d59e875db327df
nnahum13 <nim.nahum@stellus.com>
2016-05-06 12:55:40 -0400

    indentation fix

--- src/main/java/com/suse/salt/netapi/calls/modules/State.java
+++ src/main/java/com/suse/salt/netapi/calls/modules/State.java
@@ -17,42 +17,42 @@ import java.util.Optional;
  */
 public class State {
 
-	private State() {
-	}
+    private State() {
+    }
 
-	public static LocalCall<Map<String, Object>> apply(List<String> mods) {
-		LinkedHashMap<String, Object> args = new LinkedHashMap<>();
-		args.put("mods", mods);
-		return new LocalCall<>("state.apply", Optional.empty(), Optional.of(args),
-				new TypeToken<Map<String, Object>>() {});
-	}
+    public static LocalCall<Map<String, Object>> apply(List<String> mods) {
+        LinkedHashMap<String, Object> args = new LinkedHashMap<>();
+        args.put("mods", mods);
+        return new LocalCall<>("state.apply", Optional.empty(), Optional.of(args),
+                new TypeToken<Map<String, Object>>() {});
+    }
 
-	public static LocalCall<Map<String, Object>> apply(String... mods) {
-		return apply(Arrays.asList(mods));
-	}
+    public static LocalCall<Map<String, Object>> apply(String... mods) {
+        return apply(Arrays.asList(mods));
+    }
 
-	public static LocalCall<Map<String, Object>> apply(String mod, 
-	        Map<String, String> pillarArgs) {
-		if (pillarArgs == null || pillarArgs.isEmpty()) {
-			return apply(new ArrayList<String>() {
-			    {
-					add(mod);
-				}
-			});
-		}
-		LinkedHashMap<String, Object> args = new LinkedHashMap<>();
-		args.put("mods", mod);
+    public static LocalCall<Map<String, Object>> apply(String mod, 
+            Map<String, String> pillarArgs) {
+        if (pillarArgs == null || pillarArgs.isEmpty()) {
+            return apply(new ArrayList<String>() {
+                {
+                    add(mod);
+                }
+            });
+        }
+        LinkedHashMap<String, Object> args = new LinkedHashMap<>();
+        args.put("mods", mod);
 
-		if (pillarArgs != null && !pillarArgs.isEmpty()) {
-			Map<String, String> pillarData = new HashMap<>();
-			for (Entry<String, String> entry : pillarArgs.entrySet()) {
-				pillarData.put(entry.getKey(), entry.getValue());
-			}
-			args.put("pillar", pillarData);
-		}
+        if (pillarArgs != null && !pillarArgs.isEmpty()) {
+            Map<String, String> pillarData = new HashMap<>();
+            for (Entry<String, String> entry : pillarArgs.entrySet()) {
+                pillarData.put(entry.getKey(), entry.getValue());
+            }
+            args.put("pillar", pillarData);
+        }
 
-		return new LocalCall<>("state.apply", Optional.empty(), Optional.of(args),
-				new TypeToken<Map<String, Object>>() {
-				});
-	}
+        return new LocalCall<>("state.apply", Optional.empty(), Optional.of(args),
+                new TypeToken<Map<String, Object>>() {
+                });
+    }
 }
