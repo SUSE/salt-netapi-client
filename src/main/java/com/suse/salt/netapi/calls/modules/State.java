8f2f241159bccf6c9f53d441bc2d6ca6285e0980
Matei Albu <malbu@suse.de>
2016-05-10 10:16:53 +0200

    checkstyle fix

--- src/main/java/com/suse/salt/netapi/calls/modules/State.java
+++ src/main/java/com/suse/salt/netapi/calls/modules/State.java
@@ -5,7 +5,6 @@ import com.suse.salt.netapi.calls.LocalCall;
 import com.google.gson.reflect.TypeToken;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
