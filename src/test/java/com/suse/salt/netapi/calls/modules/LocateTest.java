566c9a17b4b7bc0dc825d9d3c88682ee8a354c59
Alvaro Gutierrez Romero <chiro79@gmail.com>
2016-06-06 02:20:10 +0200

    Add support for locate module
    Adds the full commands for locate module, which includes:
    - locate
    - updated
    - stats
    - version
    
    Also this commit adds a test class and an example executable class using "locate"
    command.
    
    It was tested with version 2015.8.7 which for me the locate module was not working
    and I had to fix the python "file ﻿salt/modules/locate.py":
    ﻿111,113c111,112
    <         if option in kwargs:
    <             if bool(kwargs[option]) is True:
    <                 options += toggles[option]
    ---
    >         if bool(kwargs[option]) is True:
    >             options += toggles[option]
    
    The style of the code generated tries to be coherent with the existent one.

---
+++ src/test/java/com/suse/salt/netapi/calls/modules/LocateTest.java
@@ -0,0 +1,187 @@
+package com.suse.salt.netapi.calls.modules;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.suse.salt.netapi.calls.LocalCall;
+import com.suse.salt.netapi.client.SaltClient;
+import com.suse.salt.netapi.datatypes.target.MinionList;
+import com.suse.salt.netapi.exception.SaltException;
+import com.suse.salt.netapi.utils.ClientUtils;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.Assert.*;
+
+/**
+ * Locate module unit tests.
+ */
+public class LocateTest {
+
+    private static final int MOCK_HTTP_PORT = 8888;
+
+    private static final String JSON_NULL_RESPONSE = ClientUtils.streamToString(
+            FileTest.class.getResourceAsStream(
+                    "/modules/locate/null_response.json"));
+
+    private static final String JSON_VERSION_OK_RESPONSE = ClientUtils.streamToString(
+            FileTest.class.getResourceAsStream(
+                    "/modules/locate/version_ok_response.json"));
+
+    private static final String JSON_UPDATEDB_OK_RESPONSE = ClientUtils.streamToString(
+            FileTest.class.getResourceAsStream(
+                    "/modules/locate/updatedb_ok_response.json"));
+
+    private static final String JSON_UPDATEDB_ERROR_RESPONSE = ClientUtils.streamToString(
+            FileTest.class.getResourceAsStream(
+                    "/modules/locate/updatedb_error_response.json"));
+
+    private static final String JSON_STATS_OK_RESPONSE = ClientUtils.streamToString(
+            FileTest.class.getResourceAsStream(
+                    "/modules/locate/stats_ok_response.json"));
+
+    private static final String JSON_LOCATE_OK_RESPONSE = ClientUtils.streamToString(
+            FileTest.class.getResourceAsStream(
+                    "/modules/locate/locate_ok_response.json"));
+
+    private static final String JSON_LOCATE_NOINPUT_RESPONSE = ClientUtils.streamToString(
+            FileTest.class.getResourceAsStream(
+                    "/modules/locate/locate_noinput_response.json"));
+
+
+    private SaltClient client;
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(MOCK_HTTP_PORT);
+
+    @Before
+    public void init() {
+        URI uri = URI.create("http://localhost:" + MOCK_HTTP_PORT);
+        client = new SaltClient(uri);
+    }
+
+    @Test
+    public final void testVersion() throws SaltException {
+        // First we get the call to use in the tests
+        LocalCall<List<String>> call = Locate.version();
+        assertEquals("locate.version", call.getPayload().get("fun"));
+
+        // Test for successful responses
+        stubFor(any(urlMatching("/"))
+                .willReturn(aResponse()
+                        .withStatus(HttpURLConnection.HTTP_OK)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(JSON_VERSION_OK_RESPONSE)));
+
+        Map<String, List<String>> response = call.callSync(client, new MinionList("minion1"));
+
+        assertNotNull(response.get("minion1"));
+        assertEquals(5, response.get("minion1").size());
+        assertEquals("mlocate 0.26", response.get("minion1").get(0));
+
+        // Test for null responses
+        stubFor(any(urlMatching("/"))
+                .willReturn(aResponse()
+                        .withStatus(HttpURLConnection.HTTP_OK)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(JSON_NULL_RESPONSE)));
+
+
+        response = call.callSync(client, new MinionList("minion1"));
+
+        assertNull(response.get("minion1"));
+    }
+
+    @Test
+    public final void testUpdatedb() throws SaltException {
+        // First we get the call to use in the tests
+        LocalCall<List<String>> call = Locate.updatedb();
+        assertEquals("locate.updatedb", call.getPayload().get("fun"));
+
+        // Test with a successful response
+        stubFor(any(urlMatching("/"))
+                .willReturn(aResponse()
+                        .withStatus(HttpURLConnection.HTTP_OK)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(JSON_UPDATEDB_OK_RESPONSE)));
+
+        Map<String, List<String>> response = call.callSync(client, new MinionList("minion1"));
+
+        assertNotNull(response.get("minion1"));
+        assertTrue(response.get("minion1").isEmpty());
+
+        // Test with an erroneous response
+        stubFor(any(urlMatching("/"))
+                .willReturn(aResponse()
+                        .withStatus(HttpURLConnection.HTTP_OK)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(JSON_UPDATEDB_ERROR_RESPONSE)));
+
+        response = call.callSync(client, new MinionList("minion1"));
+
+        assertNotNull(response.get("minion1"));
+        assertEquals(1, response.get("minion1").size());
+        assertEquals("updatedb:/etc/updatedb.conf:17: unknown variable `test'", response.get("minion1").get(0));
+    }
+
+    @Test
+    public final void testStats() throws SaltException {
+        // First we get the call to use in the tests
+        LocalCall<Locate.Stats> call = Locate.stats();
+        assertEquals("locate.stats", call.getPayload().get("fun"));
+
+        // Test with an erroneous response
+        stubFor(any(urlMatching("/"))
+                .willReturn(aResponse()
+                        .withStatus(HttpURLConnection.HTTP_OK)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(JSON_STATS_OK_RESPONSE)));
+
+        Map<String, Locate.Stats> response = call.callSync(client, new MinionList("minion1"));
+
+        Locate.Stats minion1 = response.get("minion1");
+        assertNotNull(minion1);
+        assertEquals(Long.valueOf(178140L), minion1.getFiles());
+        assertEquals(Long.valueOf(12070L), minion1.getDirectories());
+        assertEquals(Long.valueOf(9345681L), minion1.getFileNamesBytes());
+        assertEquals(Long.valueOf(3969350L), minion1.getDatabaseBytes());
+        assertEquals("/var/lib/mlocate/mlocate.db", minion1.getDatabaseLocation());
+    }
+
+    @Test(expected = com.google.gson.JsonSyntaxException.class)
+    public final void testLocate() throws SaltException {
+        // First we get the call to use in the tests
+        LocalCall<List<String>> call = Locate.locate("ld.so.cache", Optional.empty(), Optional.empty(), Optional.empty());
+        assertEquals("locate.locate", call.getPayload().get("fun"));
+
+        // Test with an successful response
+        stubFor(any(urlMatching("/"))
+                .willReturn(aResponse()
+                        .withStatus(HttpURLConnection.HTTP_OK)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(JSON_LOCATE_OK_RESPONSE)));
+
+        Map<String, List<String>> response = call.callSync(client, new MinionList("minion1"));
+
+        List<String> minion1 = response.get("minion1");
+        assertNotNull(minion1);
+        assertEquals(1, minion1.size());
+        assertEquals("/etc/ld.so.cache", minion1.get(0));
+
+        // Test with an exception
+        stubFor(any(urlMatching("/"))
+                .willReturn(aResponse()
+                        .withStatus(HttpURLConnection.HTTP_OK)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(JSON_LOCATE_NOINPUT_RESPONSE)));
+
+        call.callSync(client, new MinionList("minion1"));
+    }
+
+}
