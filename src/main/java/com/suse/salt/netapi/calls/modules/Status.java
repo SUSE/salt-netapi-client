7a64647051739f1fe5524f7e43691bed993934ee
Stefan Matei <smatei@gmail.com>
2016-12-20 17:12:56 +0200

    removed useless import.

--- src/main/java/com/suse/salt/netapi/calls/modules/Status.java
+++ src/main/java/com/suse/salt/netapi/calls/modules/Status.java
@@ -6,7 +6,6 @@ import com.google.gson.reflect.TypeToken;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
