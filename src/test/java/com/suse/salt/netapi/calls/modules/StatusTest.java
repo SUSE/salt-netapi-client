0152a968c52b75c2d1ae1e1c3d175f92dc981955
Stefan Matei <smatei@gmail.com>
2017-01-04 11:16:05 +0200

    added test for status.diskstats

--- src/test/java/com/suse/salt/netapi/calls/modules/StatusTest.java
+++ src/test/java/com/suse/salt/netapi/calls/modules/StatusTest.java
@@ -30,6 +30,9 @@ public class StatusTest {
 
     private static final int MOCK_HTTP_PORT = 8888;
 
+    static final String JSON_DISKSTATS_RESPONSE = ClientUtils.streamToString(
+            SaltUtilTest.class.getResourceAsStream("/modules/status/diskstats.json"));
+
     static final String JSON_UPTIME_RESPONSE = ClientUtils.streamToString(
             SaltUtilTest.class.getResourceAsStream("/modules/status/uptime.json"));
 
@@ -42,6 +45,44 @@ public class StatusTest {
     public void init() {
         URI uri = URI.create("http://localhost:" + MOCK_HTTP_PORT);
         client = new SaltClient(uri);
+    }
+
+    @Test
+    public final void testDiskstats() throws SaltException {
+        // First we get the call to use in the tests
+        LocalCall<Map<String, Map<String, Object>>> call = Status.diskstats();
+        assertEquals("status.diskstats", call.getPayload().get("fun"));
+
+        // Test with an successful response
+        stubFor(any(urlMatching("/"))
+                .willReturn(aResponse()
+                .withStatus(HttpURLConnection.HTTP_OK)
+                .withHeader("Content-Type", "application/json")
+                .withBody(JSON_DISKSTATS_RESPONSE)));
+
+        Map<String, Result<Map<String, Map<String, Object>>>> response = call.callSync(client,
+                new MinionList("minion"));
+
+        assertNotNull(response.get("minion"));
+        Map<String, Map<String, Object>> minion = response.get("minion").result().get();
+        assertNotNull(minion);
+
+        Map<String, Object> disk = minion.get("xvda1");
+        assertEquals(14, disk.size());
+        assertEquals(2.26472296E8, disk.get("ms_spent_in_io"));
+        assertEquals(2759532.0, disk.get("sectors_read"));
+        assertEquals(1.80543231E8, disk.get("weighted_ms_spent_in_io"));
+        assertEquals(4.43800912E8, disk.get("writes_completed"));
+        assertEquals(202.0, disk.get("major"));
+        assertEquals(2.5523542776E10, disk.get("sectors_written"));
+        assertEquals(1.80175149E8, disk.get("ms_spent_writing"));
+        assertEquals(4.51251256E8, disk.get("writes_merged"));
+        assertEquals(479890.0, disk.get("ms_spent_reading"));
+        assertEquals(0.0, disk.get("io_in_progress"));
+        assertEquals(117.0, disk.get("reads_merged"));
+        assertEquals("xvda1", disk.get("device"));
+        assertEquals(92229.0, disk.get("reads_issued"));
+        assertEquals(1.0, disk.get("minor"));
     }
 
     @Test
