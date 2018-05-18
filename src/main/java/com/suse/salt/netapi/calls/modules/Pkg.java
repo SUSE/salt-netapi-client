25c9a4b68ffdbdfd3333215fa04e4eee48c4bb10
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-05-18 13:06:10 +0100

    Enable Pkg.InfoInstalled to return all installed package versions

--- src/main/java/com/suse/salt/netapi/calls/modules/Pkg.java
+++ src/main/java/com/suse/salt/netapi/calls/modules/Pkg.java
@@ -259,6 +259,27 @@ public class Pkg {
                 Optional.of(kwargs), new TypeToken<Map<String, Info>>(){});
     }
 
+    /**
+     * Call 'pkg.info_installed' API.
+     *
+     * @param attributes list of attributes that should be included in the result
+     * @param reportErrors if true will return an error message instead of corrupted text
+     * @param allVersions if true will return all installed package versions, not only latest
+     * @param packages optional give package names, otherwise return info about all packages
+     * @return the call
+     */
+    public static LocalCall<Map<String, Xor<Info, List<Info>>>> infoInstalled(List<String> attributes,
+            boolean reportErrors, boolean allVersions, String... packages) {
+        LinkedHashMap<String, Object> kwargs = new LinkedHashMap<>();
+        kwargs.put("attr", attributes.stream().collect(Collectors.joining(",")));
+        kwargs.put("all_versions", allVersions);
+        if (reportErrors) {
+            kwargs.put("errors", "report");
+        }
+        return new LocalCall<>("pkg.info_installed", Optional.of(Arrays.asList(packages)),
+                Optional.of(kwargs), new TypeToken<Map<String, Xor<Info, List<Info>>>>(){});
+    }
+
     public static LocalCall<Map<String, Info>> infoAvailable(String... packages) {
         return new LocalCall<>("pkg.info_available", Optional.of(Arrays.asList(packages)),
                 Optional.empty(), new TypeToken<Map<String, Info>>(){});
