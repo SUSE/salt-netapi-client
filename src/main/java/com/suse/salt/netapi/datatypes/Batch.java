6b9dcbbcbff1581cbdba28c9fa2bc150221fdc61
Marcelo Chiaradia <mchiaradia@suse.com>
2019-02-11 13:21:28 +0100

    Add 'presence ping' timeout for salt batch calls

--- src/main/java/com/suse/salt/netapi/datatypes/Batch.java
+++ src/main/java/com/suse/salt/netapi/datatypes/Batch.java
@@ -12,26 +12,13 @@ import java.util.Optional;
 public class Batch {
     // The actual batch string
     private String batch;
-    private Optional<Double> delay = Optional.empty();
+    private Optional<Double> delay;
+    private Optional<Integer> presencePingTimeout;
 
-    private Batch(String batch) {
-        this.batch = batch;
-    }
-
-    private Batch(String batch, Double delay) {
-        this.batch = batch;
-        this.delay = Optional.of(delay);
-    }
-
-    /**
-     * Batch delay specifies the time for salt to wait for more minions to return a result before
-     * scheduling the next batch. This helps to avoid single minion batches.
-     *
-     * @param seconds time to wait in seconds.
-     * @return a copy of this batch configuration with the specified batch delay.
-     */
-    public Batch delayed(Double seconds) {
-        return new Batch(batch, seconds);
+    private Batch(BatchBuilder builder) {
+        this.batch = builder.batch;
+        this.delay = builder.delay;
+        this.presencePingTimeout = builder.presencePingTimeout;
     }
 
     @Override
@@ -45,6 +32,10 @@ public class Batch {
 
     public Optional<Double> getDelay() {
         return delay;
+    }
+
+    public Optional<Integer> getPresencePingTimeout() {
+        return presencePingTimeout;
     }
 
     public Map<String, Object> getParams() {
@@ -62,12 +53,7 @@ public class Batch {
      * @return the Batch
      */
     public static Batch asPercent(int value) {
-        if (value <= 0 || value > 100) {
-            throw new IllegalArgumentException("Expected value greater than 0 and less " +
-                    "than or equal to 100 to make valid batch as a percent.");
-        }
-
-        return new Batch(value + "%");
+        return Batch.custom().withBatchAsPercent(value).build();
     }
 
     /**
@@ -76,11 +62,96 @@ public class Batch {
      * @return the Batch
      */
     public static Batch asAmount(int value) {
-        if (value <= 0) {
-            throw new IllegalArgumentException("Expected value greater than 0 to make a " +
-                    "valid batch amount");
+        return Batch.custom().withBatchAsAmount(value).build();
+    }
+
+    /**
+     * Returns a BatchBuilder for instantiating a custom Batch.
+     *
+     * @return a BatchBuilder instance.
+     */
+    public static BatchBuilder custom() {
+        return new BatchBuilder();
+    }
+
+    /**
+     * Helper class for building a Batch.
+     */
+    public static class BatchBuilder {
+        private String batch;
+        private Optional<Double> delay;
+        private Optional<Integer> presencePingTimeout;
+
+        /**
+         * Constructor for BatchBuilder.
+         *
+         * @return a BatchBuilder instance.
+         */
+        private BatchBuilder() {
+            this.delay = Optional.empty();
+            this.presencePingTimeout = Optional.empty();
         }
 
-        return new Batch(Integer.toString(value));
+        /**
+         * Sets the batch value representing an exact amount of items
+         * @param value the exact amount of items, which must be greater than 0
+         * @return the Batch
+         */
+        public BatchBuilder withBatchAsAmount(int value) {
+            if (value <= 0) {
+                throw new IllegalArgumentException("Expected value greater than 0 to make a " +
+                        "valid batch amount");
+            }
+            this.batch = Integer.toString(value);
+            return this;
+        }
+
+        /**
+         * Sets the batch value representing a percent
+         * @param value the percent, which must be greater than 0 and less than or equal to 100
+         * @return the Batch
+         */
+        public BatchBuilder withBatchAsPercent(int value) {
+            if (value <= 0 || value > 100) {
+                throw new IllegalArgumentException("Expected value greater than 0 and less " +
+                        "than or equal to 100 to make valid batch as a percent.");
+            }
+            this.batch = Integer.toString(value);
+            return this;
+        }
+
+        /**
+         * Batch delay specifies the time for salt to wait for more minions to return a result before
+         * scheduling the next batch. This helps to avoid single minion batches.
+         *
+         * @param delayIn time to wait in seconds.
+         * @return this BatchBuilder instance with the specified batch delay.
+         */
+        public BatchBuilder withDelay(Double delayIn) {
+            this.delay = Optional.of(delayIn);
+            return this;
+        }
+
+        /**
+         * Batch presence ping timeout specifies the timeout in seconds of the presence ping performed in
+         * salt minions to determine which minions are available during salt batch calls.
+         *
+         * @param presencePingTimeoutIn time to wait in seconds.
+         * @return this BatchBuilder instance with the specified batch presence ping timeout.
+         */
+        public BatchBuilder withPresencePingTimeout(Integer presencePingTimeoutIn) {
+            this.presencePingTimeout = Optional.of(presencePingTimeoutIn);
+            return this;
+        }
+
+        /**
+         * Returns a Batch with the values of this BatchBuilder instance.
+         *
+         * @return a new Batch
+         */
+        public Batch build() {
+            return new Batch(this);
+        }
+
     }
 }
