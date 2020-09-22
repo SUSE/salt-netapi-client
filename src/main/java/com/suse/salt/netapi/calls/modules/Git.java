2f9c98af7f657572185dff38624683b8b5b858df
Francisco Revilla <paconte@gmail.com>
2020-09-22 19:32:41 +0200

    Avoid UnsopportedOperationException when inserting elements in a fixed size list.

--- src/main/java/com/suse/salt/netapi/calls/modules/Git.java
+++ src/main/java/com/suse/salt/netapi/calls/modules/Git.java
@@ -4,7 +4,6 @@ import com.suse.salt.netapi.calls.LocalCall;
 import com.suse.salt.netapi.results.GitResult;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,7 +19,7 @@ public class Git {
     private Git() { }
 
     public static LocalCall<GitResult> status(String cwd, Optional<String> user) {
-        List<String> args = Arrays.asList(cwd);
+        List<String> args = new ArrayList<>(Arrays.asList(cwd));
         user.ifPresent(usr -> args.add(usr));
         return new LocalCall<>("git.status", Optional.of(args),
                 Optional.of(emptyMap()), new TypeToken<GitResult>(){});
@@ -28,7 +27,7 @@ public class Git {
 
     public static LocalCall<String> add(String cwd, String filename,
             String opts, String gitOpts, Optional<String> user) {
-        List<String> args = Arrays.asList(cwd, filename, opts, gitOpts);
+        List<String> args = new ArrayList<>(Arrays.asList(cwd, filename, opts, gitOpts));
         user.ifPresent(usr -> args.add(usr));
         return new LocalCall<>("git.add", Optional.of(args),
                 Optional.of(emptyMap()), new TypeToken<String>(){});
@@ -36,7 +35,7 @@ public class Git {
 
     public static LocalCall<String> commit(String cwd, String message,
             String opts, String gitOpts, Optional<String> user, Optional<String> filename) {
-        List<String> args = Arrays.asList(cwd, message, opts, gitOpts);
+        List<String> args = new ArrayList<>(Arrays.asList(cwd, message, opts, gitOpts));
         user.ifPresent(usr -> args.add(usr));
         filename.ifPresent(file -> args.add(file));
         return new LocalCall<>("git.commit", Optional.of(args),
@@ -45,7 +44,7 @@ public class Git {
 
     public static LocalCall<Boolean> branch(String cwd, String branch,
             String opts, String gitOpts, Optional<String> user) {
-        List<String> args = Arrays.asList(cwd, branch, opts, gitOpts);
+        List<String> args = new ArrayList<>(Arrays.asList(cwd, branch, opts, gitOpts));
         user.ifPresent(usr -> args.add(usr));
         return new LocalCall<>("git.branch", Optional.of(args),
                 Optional.of(emptyMap()), new TypeToken<Boolean>(){});
