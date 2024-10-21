5cee6892dcfcb8ced87534ab20d5b3514f012b65
Michael Calmer <michael.calmer@suse.com>
2024-10-21 13:23:03 +0200

    implement withExecutors for LocalCall

--- src/test/java/com/suse/salt/netapi/calls/LocalCallTest.java
+++ src/test/java/com/suse/salt/netapi/calls/LocalCallTest.java
@@ -38,6 +38,8 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -122,6 +124,34 @@ public class LocalCallTest {
         assertEquals(runWithTimeouts.getPayload().get("timeout"), 4);
         assertEquals(runWithTimeouts.getPayload().get("gather_job_timeout"), 1);
     }
+
+    @Test
+    public void testWithExecutors() {
+        LocalCall<String> run = Cmd.run("echo 'hello world'");
+        assertFalse(run.getPayload().containsKey("module_executors"));
+        assertFalse(run.getPayload().containsKey("executor_opts"));
+
+        LocalCall<String> runWithExecutors = run.withExecutors(Optional.of(List.of("direct_call")), Optional.empty());
+        assertTrue(runWithExecutors.getPayload().containsKey("module_executors"));
+        assertEquals(List.of("direct_call"), runWithExecutors.getPayload().get("module_executors"));
+        assertFalse(runWithExecutors.getPayload().containsKey("executor_opts"));
+
+        runWithExecutors = runWithExecutors.withExecutors(Optional.of(List.of("splay")),
+                Optional.of(Map.of("splaytime", 30)));
+        assertTrue(runWithExecutors.getPayload().containsKey("module_executors"));
+        assertEquals(List.of("splay"), runWithExecutors.getPayload().get("module_executors"));
+        assertTrue(runWithExecutors.getPayload().containsKey("executor_opts"));
+        assertEquals(Map.of("splaytime", 30), runWithExecutors.getPayload().get("executor_opts"));
+
+        LocalCall<String> runWithoutExecutors = runWithExecutors.withoutExecutors();
+        assertFalse(runWithoutExecutors.getPayload().containsKey("module_executors"));
+        assertFalse(runWithoutExecutors.getPayload().containsKey("executor_opts"));
+        assertTrue(runWithExecutors.getPayload().containsKey("module_executors"));
+        assertEquals(List.of("splay"), runWithExecutors.getPayload().get("module_executors"));
+        assertTrue(runWithExecutors.getPayload().containsKey("executor_opts"));
+        assertEquals(Map.of("splaytime", 30), runWithExecutors.getPayload().get("executor_opts"));
+    }
+
     /**
      * Verify that system return the correct module name and function name
      */
